39a89cea67e81a81c71a8d75e943d03b66b9b1a7
Pinto Jody <pintojody20@gmail.com>
2022-11-14 13:34:48 +0100

    add customJWT for get user id

---
+++ src/EventListener/JWTCreatedListener.php
@@ -0,0 +1,43 @@
+<?php 
+namespace App\EventListener;
+
+use Lexik\Bundle\JWTAuthenticationBundle\Event\JWTCreatedEvent;
+use Symfony\Component\HttpFoundation\RequestStack;
+
+class JWTCreatedListener{
+
+/**
+ * @var RequestStack
+ */
+private $requestStack;
+
+/**
+ * @param RequestStack $requestStack
+ */
+public function __construct(RequestStack $requestStack)
+{
+    $this->requestStack = $requestStack;
+}
+
+/**
+ * @param JWTCreatedEvent $event
+ *
+ * @return void
+ */
+public function onJWTCreated(JWTCreatedEvent $event)
+{
+    $request = $this->requestStack->getCurrentRequest();
+
+    $payload       = $event->getData();
+    $user          = $event->getUser();
+    $payload['id'] = $user->getId();
+
+
+    $event->setData($payload);
+
+    $header        = $event->getHeader();
+    $header['cty'] = 'JWT';
+
+    $event->setHeader($header);
+}
+}
